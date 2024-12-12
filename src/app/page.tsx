aa05a1cad8b75233a578288edec26ca9228928e2
Martins Siksnis <msiksnis@gmail.com>
2024-12-12 17:03:24 +0100

    added gradient sides for running marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,12 @@ import ContactSection from "@/components/contact/ContactSection";
 import FavoriteTools from "@/components/FavoriteTools";
 import HomeSection from "@/components/HomeSection";
 import PortfolioSection from "@/components/portfolio/PortfolioSection";
-import Marquee from "@/components/ui/marquee";
 
 export default function HomePage() {
   return (
     <div className="mx-auto max-w-4xl space-y-4 pb-10">
       <HomeSection />
-      <Marquee pauseOnHover className="[--duration:50s]">
-        <FavoriteTools />
-      </Marquee>
+      <FavoriteTools />
       <PortfolioSection />
       <ContactSection />
     </div>
--- src/components/FavoriteTools.tsx
+++ src/components/FavoriteTools.tsx
@@ -1,3 +1,5 @@
+import Marquee from "./ui/marquee";
+
 const techStack = [
   "JavaScript",
   "React",
@@ -12,15 +14,21 @@ const techStack = [
 
 export default function FavoriteTools() {
   return (
-    <div className="flex flex-wrap gap-2">
-      {techStack.map((tool) => (
-        <div
-          key={tool}
-          className="w-fit rounded-4xl border border-amber-200 px-3 py-0.5 text-amber-200"
-        >
-          {tool}
+    <div className="relative">
+      <Marquee pauseOnHover className="[--duration:50s]">
+        <div className="flex flex-wrap gap-2">
+          {techStack.map((tool) => (
+            <div
+              key={tool}
+              className="w-fit rounded-4xl border border-amber-200 px-3 py-0.5 text-amber-200"
+            >
+              {tool}
+            </div>
+          ))}
         </div>
-      ))}
+      </Marquee>
+      <div className="absolute left-0 top-0 h-12 w-6 bg-gradient-to-r from-primary to-transparent"></div>
+      <div className="absolute right-0 top-0 h-12 w-6 bg-gradient-to-r from-transparent to-primary"></div>
     </div>
   );
 }
