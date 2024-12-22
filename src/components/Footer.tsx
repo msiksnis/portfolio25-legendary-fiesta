2c7ebcc36f28fb7fe63745406665071990da5c2d
Martins Siksnis <msiksnis@gmail.com>
2024-12-22 22:40:35 +0100

    fixed wrong border for the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { MagicCard } from "./ui/magic-card";
 
 export default function Footer() {
   return (
-    <div className="mx-auto mb-10 flex w-full max-w-4xl justify-center rounded-4xl border bg-card">
+    <div className="mx-auto mb-10 flex w-full max-w-4xl justify-center rounded-4xl border border-border bg-card">
       <MagicCard className="items-center justify-center rounded-4xl px-16 py-4">
         <div className="text-muted-foreground">
           All rights reserved. © 2024
