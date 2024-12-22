6210a901e66c6c6837f0ecc064944342810bdfd4
Martins Siksnis <msiksnis@gmail.com>
2024-12-22 22:40:05 +0100

    fixed wrong border for the avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Avatar() {
   return (
-    <div className="absolute left-1/2 top-1/2 z-20 -mt-[3px] hidden -translate-x-1/2 -translate-y-1/2 rounded-full border sm:flex">
+    <div className="absolute left-1/2 top-1/2 z-20 -mt-[3px] hidden -translate-x-1/2 -translate-y-1/2 rounded-full border border-border sm:flex">
       <div className="relative">
         <div className="absolute -top-2 left-1/2 h-3 w-6 -translate-x-1/2 bg-primary"></div>
         <div className="absolute -left-2 top-1/2 h-6 w-3 -translate-y-1/2 bg-primary"></div>
